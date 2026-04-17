f646975cd8401985bf8b61c22a2201eb94554165
weapub <weapublicidad@gmail.com>
2026-04-17 01:02:21 -0300

    Use implicit OAuth flow for SPA

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -8,6 +8,6 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     persistSession: true,
     autoRefreshToken: true,
     detectSessionInUrl: true,
-    flowType: 'pkce',
+    flowType: 'implicit',
   },
 })
